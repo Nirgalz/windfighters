c737c0efa773e7992e00b431a6a4edf9e1460785
Nirgalz <znirgal@gmail.com>
2016-06-03 21:30:32 +0200

    update

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-var tank1 = [100],
-    tank2 = [100,],
-    tank3 = [100,],
-    healer = [100,],
-    scout = [100,],
-    fighter =[100,];
+var tank1 = [100, "tank1"],
+    tank2 = [100, "tank2"],
+    tank3 = [100, "tank3"],
+    healer = [100, "healer"],
+    scout = [100, "scout"],
+    fighter =[100, "fighter"];
 
 
 function stop() {
     clearInterval(timer);
 }
 
-function start(){
+function start(hordeux){
     timer = setInterval(wind, 1000);
+
+    function wind() {
+    hordeux[0]--;
+    console.log(hordeux[0]);
+    }
 }
 
 
-function wind() {
-    tank1[0]--;
-    console.log(tank1[0]);
-}
+
