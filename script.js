e6a1b96263c0a90d7bdf1ff658b43736d011d739
Nirgalz <znirgal@gmail.com>
2016-06-03 21:52:30 +0200

    added heal

--- script.js
+++ script.js
@@ -3,20 +3,36 @@ var tank1 = [100, "tank1"],
     tank3 = [100, "tank3"],
     healer = [100, "healer"],
     scout = [100, "scout"],
-    fighter =[100, "fighter"];
+    fighter =[100, "fighter"],
+    events = [];
 
 
-function stop() {
-    clearInterval(timer);
-}
 
-function start(hordeux){
-    timer = setInterval(wind, 1000);
 
-    function wind() {
+/*funcation damage wind force 1, may not be scalable*/
+function startWind1(hordeux) {
+    timerWind1 = setInterval(wind1, 1000);
+    function wind1() {
     hordeux[0]--;
     console.log(hordeux[0]);
     }
+}
+/*stops damage*/
+function stopWind1() {
+    clearInterval(timerWind1);
+}
+
+/*function heal, may not be scalable as well*/
+function heal(hordeux) {
+    timerHeal = setInterval(heal, 1500);
+    function heal() {
+        hordeux[0]++;
+        console.log(hordeux[0]);
+    }
+}
+/*stop heal*/
+function stopHeal() {
+    clearInterval(timerHeal);
 }
 
 
