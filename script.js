4991f38a2bab94804cfc5fdb72dbb0d4a92c96ee
Nirgalz <znirgal@gmail.com>
2016-06-03 16:55:59 +0200

    few tests

---
+++ script.js
@@ -0,0 +1,27 @@
+var tank1 = [10],
+    tank2 = [100,],
+    tank3 = [100,],
+    healer = [100,],
+    scout = [100,],
+    fighter =[100,];
+
+function couille(){
+    var timer = setInterval(wind, 1000);
+}
+
+
+function wind() {
+    var d = new Date();
+    console.log(d.toLocaleTimeString);
+}
+
+
+
+/*function wind1(horder){
+    console.log(horder[0]);
+    for (horder[0]; horder[0] >= 0; horder[0]66) {
+    
+            console.log(horder);
+    
+    }
+}*/
